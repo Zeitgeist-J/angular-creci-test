dc529f4a5716bb430ca5be195d0ab4490765f7d2
Zeitgeist-J <shecksk8nick@gmail.com>
2019-10-11 16:44:45 -0500

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MovieComponent } from './core/movie/movie.component';
+
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, MovieComponent, ],
+  declarations: [ AppComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
