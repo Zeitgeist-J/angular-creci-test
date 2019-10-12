b015fe84794a9f38ff7efb49f396f311a876041a
Zeitgeist-J <shecksk8nick@gmail.com>
2019-10-12 17:38:30 -0500

    Including firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,30 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './Core/top-bar/top-bar.component';
 import { MovieComponent } from './Core/movie/movie.component';
 import { MoviesService } from './movies.service';
-import { CollectionComponent } from './Core/collection/collection.component';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
 
+
+var config = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
+};
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule,
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
   
   RouterModule.forRoot([
     { path: 'Home', component: AppComponent },
   ])
   
   ],
-  declarations: [ AppComponent, TopBarComponent, MovieComponent, CollectionComponent ],
+  declarations: [ AppComponent, TopBarComponent, MovieComponent ],
   bootstrap:    [ AppComponent ],
   providers: [MoviesService]
 })
